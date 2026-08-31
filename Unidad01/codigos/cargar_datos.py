19f29190d187d0393146477461b124827cef67b1
larigalvan096-sketch <larigalvan096@gmail.com>
2026-08-31 13:57:39 -0600

    Create cargar_datos.py

---
+++ Unidad01/codigos/cargar_datos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+link_csv = "https://raw.githubusercontent.com/larigalvan096-sketch/Estadisticainferencial2026A/refs/heads/main/Unidad01/problemario/datos_problema_01.csv"
+
+df = pd. read_csv(link_csv)
+
+df
